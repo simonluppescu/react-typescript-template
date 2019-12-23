f88f21ad720130b92c9f95bf40500f1d638899e9
Simon Luppescu <simon.luppescu@gmail.com>
2019-12-23 22:33:28 +0900

    Added provider to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import React, { FC } from "react";
+import { Provider } from "react-redux";
+
 import "./App.css";
 
+import { store } from "./store/configureStore";
+
 const App: FC = () => {
-  return <div>Hello World!</div>;
+  return (
+    <Provider store={store}>
+      <div>Hello World!</div>
+    </Provider>
+  );
 };
 
 export default App;

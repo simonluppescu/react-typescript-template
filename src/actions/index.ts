2a5da127128f1cdf9c49504ee7fdb34292ef11f0
Simon Luppescu <simon.luppescu@gmail.com>
2021-01-04 00:18:19 +0900

    Tentative additions

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,6 +1,22 @@
+import axios from "axios";
 import { ActionNames, MyAction } from "../types/actionData";
+import { AppAsyncAction } from "../types/common";
 
 export const doSomething = (someField: string): MyAction => ({
   type: ActionNames.MY_ACTION
   // someField
+});
+
+export const fetchData = (someField: string): AppAsyncAction => async (dispatch, getState): Promise<any> => {
+  try {
+    const response = await axios.get("https://www.example.com/api");
+  } catch (e) {
+    // Do something with error
+  }
+};
+
+// Adjust this to use correct actions
+export const setData = (someField: string): MyAction => ({
+  type: ActionNames.MY_ACTION,
+  someField
 });

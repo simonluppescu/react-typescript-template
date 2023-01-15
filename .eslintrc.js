afc752ec12d8c4de09b99c38a2102f453a1c5d0e
Simon <simon.luppescu@gmail.com>
2023-01-15 18:03:49 +0900

    Upgraded some eslint packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,29 @@
 module.exports = {
   parser: "@typescript-eslint/parser", // Specifies the ESLint parser
   extends: [
-    "plugin:@typescript-eslint/recommended", // Uses the recommended rules from the @typescript-eslint/eslint-plugin
-    "prettier/@typescript-eslint", // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
-    "plugin:prettier/recommended", // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+    // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+    "plugin:@typescript-eslint/recommended",
+    // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin
+    // that would conflict with prettier
+    "prettier",
+    // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors.
+    // Make sure this is always the last configuration in the extends array.
+    "plugin:prettier/recommended"
   ],
   parserOptions: {
     ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
-    sourceType: "module", // Allows for the use of imports
+    sourceType: "module" // Allows for the use of imports
   },
   rules: {
     indent: "off",
+    "max-len": ["error", { code: 120 }],
     "no-invalid-this": "off",
     "no-unused-vars": "off",
     "@typescript-eslint/no-unused-vars": "off",
     "require-jsdoc": "off",
     "@typescript-eslint/no-use-before-define": "off",
     "@typescript-eslint/no-empty-function": "off",
-    "@typescript-eslint/no-explicit-any": "off"
-  },
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/camelcase": "off"
+  }
 };
